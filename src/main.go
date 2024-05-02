6d819a2397361012b44e387785723b6de29d6a64
Carlos Guajardo C <cj.guajardo@cgcapps.cl>
2024-05-02 09:27:49 -0400

    improvement on convetion flow process

--- src/main.go
+++ src/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"os/exec"
 	"strings"
@@ -26,63 +24,48 @@ func main() {
 		"https://rie2024.s3.amazonaws.com/v_171451781128567964.audio.webm",
 	}
 
-	for _, url := range urls {
-		// download the file
-		localFile, err := downloadFile(url)
-		if err != nil {
-			fmt.Println("Error downloading file: ", err)
-		} else {
-			if strings.Contains(url, ".audio.") {
-				convertedFile := fmt.Sprintf("output/%s.mp3", strings.Split(localFile, ".")[0])
-				out, err := os.Create(convertedFile)
-				if err != nil {
-					fmt.Println("Error creating the file:", err)
-				} else {
-					defer out.Close()
-					fmt.Println("Audio file detected")
-					command := fmt.Sprintf("/usr/bin/ffmpeg -i %s -vn -c:a libmp3lame -q:a 2 %s", localFile, convertedFile)
-					cmd := exec.Command(command)
-					if err := cmd.Run(); err == nil {
-						fmt.Println("File converted: ", convertedFile)
-					} else {
-						fmt.Println("Error converting file: ", err)
-					}
-				}
-			} else {
-				fmt.Println("Audio file not detected")
-			}
-		}
-	}
+  for _, url := range urls {
+    fileName := getFileName(url)
+    destFile:=""
+    if strings.Contains(url, ".audio.") {
+      destFile = strings.ReplaceAll(fileName, ".webm", ".mp3")
+    } else {
+      destFile = strings.ReplaceAll(fileName, ".webm", ".mp4")
+    }
 
+    if destFile != "" {
+      out, err := os.Create(destFile)
+      if err != nil {
+        fmt.Println("Error creating the file:", err)
+      }
+        defer out.Close()
+
+      convert(url, destFile)					
+    }
+  }
 }
 
-func downloadFile(url string) (string, error) {
-	fmt.Println("Downloading file: ", url)
-	response, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Error downloading file: ", err)
-		return "", err
-	}
+func convert(sourceFile string, destFile string) {
+  params := " "
+  if strings.Index(destFile, ".mp4") != -1 {
+    params=" -c:v libx264 -q:v 15 "
+  }
+  args := fmt.Sprintf("-loglevel fatal -i %s -c:a libmp3lame%s/app/output/%s -y", sourceFile, params, destFile)
+  fmt.Println("ffmpeg " + strings.ReplaceAll(args,"/app/",""))
 
-	defer response.Body.Close()
+  cmd := exec.Command("/usr/bin/ffmpeg", args)
+  err:=cmd.Run()
+  if err != nil {
+    fmt.Println("Error converting file: ", err)
+  } else {
+    fmt.Println("File converted: ", destFile)
+  }
+}
 
+func getFileName(url string) string  {
 	// get file name
 	parts := strings.Split(url, "/")
-	localFile := parts[len(parts)-1]
-	// Create the file
-	out, err := os.Create(localFile)
-	if err != nil {
-		fmt.Println("Error creating the file:", err)
-		return "", err
-	}
-	defer out.Close()
+  fileName := parts[len(parts)-1]
 
-	// Copy the content from the HTTP response to the file
-	_, err = io.Copy(out, response.Body)
-	if err != nil {
-		fmt.Println("Error copying content to file:", err)
-		return "", err
-	}
-
-	return localFile, nil
+	return fileName 
 }
