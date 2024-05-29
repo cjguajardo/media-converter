c5f1b223fc59c731e1684d9cce0e81bbc0a7cebb
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2024-05-29 11:30:14 -0400

    chore: Refactor router.js for improved code organization and readability

--- src_node/router.js
+++ src_node/router.js
@@ -1,40 +1,40 @@
-import express from 'express';
-import multer from 'multer';
-import os from 'os';
-import converterController from './controllers/converter/index.js';
-import auth from './controllers/auth/index.js';
-import JWT from './utils/jwt.js';
+import express from 'express'
+import multer from 'multer'
+import os from 'os'
+import converterController from './controllers/converter/index.js'
+import authController from './controllers/auth/index.js'
+import JWT from './utils/jwt.js'
 
-const upload = multer({ dest: os.tmpdir() });
-const router = express.Router();
+const upload = multer({ dest: os.tmpdir() })
+const router = express.Router()
 
-const protected_urls = ['POST::/convert', 'POST::/'];
+const protected_urls = ['POST::/convert']
 
 router.use((req, res, next) => {
-  const { url, method } = req;
-  console.log({ url, method });
+  const { url, method } = req
+  console.log({ url, method })
   if (protected_urls.indexOf(`${method}::${url}`) != -1) {
-    const token = req.headers['authorization']?.replace('Bearer ', '');
-    const jwt = new JWT();
-    const valid_token = jwt.check(token);
+    const token = req.headers['authorization']?.replace('Bearer ', '')
+    const jwt = new JWT()
+    const valid_token = jwt.check(token)
     if (!valid_token) {
-      return res.sendStatus(403);
+      return res.sendStatus(403)
     }
   }
-  next();
-});
+  next()
+})
 
 router.get('/status', (_, res) => {
-  return res.status(200).json({ message: 'Server is running' });
-});
+  return res.status(200).json({ message: 'Server is running' })
+})
 
 router.get('/recorder-test', (_, res) => {
-  return res.sendFile(`/app/html/index.html`);
-});
+  return res.sendFile(`/app/html/index.html`)
+})
 
-router.post('/', upload.single('file'), converterController.static);
-router.post('/convert', upload.single('file'), converterController.auto);
+router.post('/', upload.single('file'), converterController.static)
+router.post('/convert', upload.single('file'), converterController.auto)
 
-router.post('/login', auth.login);
+router.post('/login', authController.login)
 
-export default router;
+export default router
