23f82b79314052a230c85a06ef48f77fa70ec0fc
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2024-06-11 12:09:23 -0400

    chore: Refactor S3 upload logic and add content type
    - Refactor the S3 upload logic in the `upload` function
    - Add the `mimeType` parameter to specify the content type of the uploaded file

--- src_node/utils/s3.js
+++ src_node/utils/s3.js
@@ -1,4 +1,4 @@
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 
 const client = new S3Client({
   region: process.env.AWS_REGION,
@@ -6,31 +6,32 @@ const client = new S3Client({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCES_KEY,
   },
-});
+})
 
-export const upload = async ({ destFileName, fileContent }) => {
+export const upload = async ({ destFileName, fileContent, mimeType = "application/octet-stream" }) => {
   return new Promise((resolve, reject) => {
     const command = new PutObjectCommand({
       ACL: 'public-read',
       Bucket: process.env.AWS_BUCKET_NAME,
       Key: destFileName,
       Body: fileContent,
-    });
+      ContentType: mimeType,
+    })
 
     client
       .send(command)
       .then((response) => {
         // console.log({ response });
-        const url = `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${destFileName}`;
+        const url = `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${destFileName}`
         resolve({
           message: 'File uploaded successfully',
           requestId: response.$metadata.requestId,
           url,
-        });
+        })
       })
       .catch((err) => {
-        console.error(err);
-        reject(err);
-      });
-  });
-};
+        console.error(err)
+        reject(err)
+      })
+  })
+}
