10e45f942c42181aa0914c099bde7a900efc3052
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2024-08-20 12:28:40 -0400

    feat: added cors management from env vars

--- src_node/index.js
+++ src_node/index.js
@@ -2,9 +2,16 @@ import express from 'express'
 import router from './router.js'
 import { init } from './utils/db.js'
 
+origins = process.env.ALLOWED_ORIGINS?.split(',') ?? '*'
+
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.all('*', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", origins)
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 // After all other middleware and routes
 app.use((err, req, res, next) => {
   console.error(err.stack) // Log error stack to console
